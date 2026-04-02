fe3f75204d58346d656029696b8054ab396a4a76
zuzannapajak <120385863+zuzannapajak@users.noreply.github.com>
2026-04-03 00:16:11 +0200

    Configuration for network run dev

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.11", "localhost", "127.0.0.1"],
 };
 
 const withNextIntl = createNextIntlPlugin();
